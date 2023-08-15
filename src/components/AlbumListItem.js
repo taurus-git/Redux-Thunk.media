3bed48d93e4fd5bf9d07bdad621e325b43cfe1f3
isupov <t.1113@ya.ru>
2023-08-15 09:31:51 +0300

    Added RTQ

---
+++ src/components/AlbumListItem.js
@@ -0,0 +1,28 @@
+import { GoTrash } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store"
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import PhotosList from "./PhotosList";
+
+function AlbumListItem( { album } ) {
+    const [removeAlbum, results] = useRemoveAlbumMutation();
+
+    const handleRemoveAlbum = () => {
+        removeAlbum( album );
+    }
+
+    const header = <>
+        <Button className="mr-2" loading={ results.isLoading } onClick={ handleRemoveAlbum }>
+            <GoTrash/>
+        </Button>
+        { album.title }
+    </>;
+
+    return (
+        <ExpandablePanel key={ album.id } header={ header }>
+            <PhotosList album={ album }/>
+        </ExpandablePanel>
+    );
+}
+
+export default AlbumListItem;
--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import AlbumListItem from "./AlbumListItem";
 
 function AlbumsList( { user } ) {
+    const { data, error, isFetching } = useFetchAlbumsQuery( user );
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum( user );
+    }
+
+    let content;
+    if ( isFetching ) {
+        content = <Skeleton className="h-10 w-full" times={ 3 }/>
+    } else if ( error ) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map( album => {
+            return <AlbumListItem key={ album.id } album={ album }/>
+        } )
+    }
+
     return <div>
-        Albums for { user.name }
+        <div className="m-2 flex flex-row items-center justify-between">
+            <h3 className="text-lg font-bold">Albums for { user.name }</h3>
+            <Button loading={ results.isLoading } onClick={ handleAddAlbum }>
+                + Add Album
+            </Button>
+        </div>
+        <div>{ content }</div>
     </div>;
 }
 
